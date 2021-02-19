4f7de86f04a1e9e5bf17b6f4bb886ddeb504466d
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-02-19 12:08:03 -0300

    Estruturando json de erro

--- src/main/java/org/acme/exception/MyApplicationExceptionHandler.java
+++ src/main/java/org/acme/exception/MyApplicationExceptionHandler.java
@@ -1,11 +1,19 @@
 package org.acme.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+
+import org.acme.dto.Meta;
+import org.acme.dto.ResponseError;
+import org.acme.dto.ResponseErrorContainer;
+import org.acme.dto.ResponseErrorList;
 
 import io.vertx.core.json.JsonObject;
 
@@ -16,8 +24,17 @@ public class MyApplicationExceptionHandler implements ExceptionMapper<MyApplicat
 	@Override
 	public Response toResponse(MyApplicationException exception) 
 	{
-		JsonObject json = new JsonObject()
-    	        .put("Errors", exception.getMessage());
-		return Response.status(Status.BAD_REQUEST).entity(json).build();	
+		
+		ResponseError  error = new ResponseError("teste", "Ocorreu um erro", exception.getMessage());
+		List<ResponseError> errors = new ArrayList<ResponseError>();
+		errors.add(error);
+		
+		ResponseErrorList responseErrorList = ResponseErrorList.builder().errors(errors).build();
+		Meta meta = new Meta();
+		
+		//ResponseErrorContainer.builder().errors(responseErrorList).meta(meta).build();
+		
+		return Response.status(Status.BAD_REQUEST).entity(ResponseErrorContainer.builder().errors(responseErrorList).meta(meta).build()).build();
+		
 	}
 }
